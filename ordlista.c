9ad3390438906799895733a65da374298659a434
Voroz <jonny_fherm@hotmail.com>
2015-11-19 23:41:53 +0100

    Fixed broken allocation in storeFileWords() function. Implemented findWord function.

--- ordlista.c
+++ ordlista.c
@@ -3,20 +3,23 @@
 #include <math.h>
 #include <string.h>
 
+const int maxWordLength = 100;
+
 typedef struct{
     char* string;
-    size_t size;
+    int size;
 }Words;
 
-void storeFileWords(char* filename, Words *words){
+Words* storeFileWords(char* filename){
     //Read from file
     FILE *file;
     if (!(file = fopen(filename, "r"))){
         printf("Error opening %s\n", filename);
-        return 0;
+        return;
     }
 
     //Read size and allocate memory
+    Words *words;
     int arraySize = 0;
     fscanf(file, "%d //Amount of words\n", &arraySize);
     words = (Words*)malloc(arraySize*sizeof(Words));
@@ -24,14 +27,18 @@ void storeFileWords(char* filename, Words *words){
     words[0].size = arraySize;
 
     //Allocate memory for string length and save in array
-    char *currentString = (char*)malloc(100*sizeof(char)); //Temp memory
-    for (int i = 1; fscanf(file, "%s\n", currentString) != EOF; i++){
-        words[i].size = strlen(currentString);
-        words[i].string = (char*)realloc(currentString, words[i].size * sizeof(char*));
-        words[i].string = currentString;
+    words[1].string = (char*)malloc(maxWordLength*sizeof(char));
+    for (int i = 1; fscanf(file, "%s\n", words[i].string) != EOF; i++){
+        words[i].size = strlen(words[i].string);
+        //Reallocate memory to match word size
+        words[i].string = (char*)realloc(words[i].string, words[i].size * sizeof(char*));
+        //Allocate room for next word (unless we are at last word)
+        if (i < arraySize-1){
+            words[i+1].string = (char*)malloc(maxWordLength * sizeof(char*));
+        }
     }
     fclose(file);
-    return;
+    return words;
 }
 
 void addWord(char* word, int position){
@@ -39,12 +46,14 @@ void addWord(char* word, int position){
 }
 
 //Find word and return word position
-int findWord(char* word){
-    int position;
-    /*
-    ...Code
-    */
-    return position;
+int findWord(char* word, Words *words){
+    for (int i = 1; i < words[0].size; i++){
+        //Check if strings match with memcmp
+        if (memcmp(word, words[i].string, words[i].size) == 0){
+            return i; // i = position
+        }
+    }
+    return 0;
 }
 
 //Move word down 1 position
@@ -58,7 +67,8 @@ int main(){
 
 Words *words;
 
-storeFileWords("ordlista.txt", words);
+words = storeFileWords("ordlista.txt");
+printf("%d\n", findWord("Allrum", words));
 
 
 return 0;
