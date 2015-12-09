e53e34626ad29b0fd448b33ee690ffd793380cf7
Voroz <jonny_fherm@hotmail.com>
2015-12-09 19:37:59 +0100

    Create vector.h

---
+++ vector.h
@@ -0,0 +1,33 @@
+typedef struct{
+	int size;		// Slots used so far
+	int capacity;	// Total available slots
+	void **data;	// Array of data we're storing
+	int changes;	// Keeps track of user changes to the vector
+} Vector;
+
+void vectorInit(Vector *pVector);
+
+int vectorSize(Vector *pVector);
+
+static void** vectorCopyValue(void* *value, int sizeOfElem);
+
+static void vectorFreeValue(void* value);
+
+static void vectorDoubleCapacityIfFull(Vector *pVector);
+
+static void vectorHalfCapacityIfNotUsed(Vector *pVector);
+
+void vectorAppend(Vector *pVector, void* *value, int sizeOfElem);
+
+void vectorSet(Vector *pVector, int index, void* *value);
+
+void* vectorGet(Vector *pVector, int index);
+
+// Moves all values one step higer from index position and inserts the new value at index
+void vectorInsert(Vector *pVector, int index, void* *value, int sizeOfElem);
+
+void vectorRemove(Vector *pVector, int index);
+
+void vectorFree(Vector *pVector);
+
+void vectorClear(Vector *pVector);
